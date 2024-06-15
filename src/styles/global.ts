b14c0673738a055955b698a17fe8e5b1255f2e93
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-06-15 11:50:23 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { globalCss } from "@marcosvinicius-ignite-ui/react"
+
+export  const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+
+  body: {
+    backgroundColor: "$gray900",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+  }
+})
